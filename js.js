0ee8d4818ac9e62c9baff4a03be9e47bf3881f1a
12pankaj <121386091+12pankaj@users.noreply.github.com>
2023-05-18 22:42:28 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,29 @@
+function changemu() {
+                   var mu = document.getElementById('mu');
+                  var mus = (mu.setAttribute("src","image/musddd.gif"))
+            }
+            function chang()
+            {
+                 var mu = document.getElementById('mu');
+                 var mus = (mu.setAttribute("src","image/mus.gif"))
+            }
+            function changetr() {
+                   var tr = document.getElementById('tr');
+                var trs = (tr.setAttribute("src","image/treval.gif"))
+            }
+            function changtr()
+            {
+                 var tr = document.getElementById('tr');
+                var trs = (tr.setAttribute("src","image/trevals.gif"))
+            }
+             
+            function changeco() {
+                   var co = document.getElementById('co');
+                var cos = (co.setAttribute("src","image/co.gif"))
+            }
+            function changco()
+            {
+                 var co = document.getElementById('co');
+                var cos = (co.setAttribute("src","image/cos.gif"))
+            }
+
